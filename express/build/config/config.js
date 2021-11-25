b1f9d9f86781528a80495de94c4c35a8f115c160
童锴 <346762712@qq.com>
2021-11-25 09:59:03 +0800

    fix: rename express to server

--- express/build/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: "CHANGEME!!!",
-    issuer: "accounts.codetek.eu",
-    audience: "codetek.eu",
-    expiration: "7d",
-};
-//# sourceMappingURL=config.js.map
--- express/build/utils/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    Admin: "ADMIN",
-    Moderator: "MODERATOR",
-    USER: "USER",
-};
-//# sourceMappingURL=roles.js.map
